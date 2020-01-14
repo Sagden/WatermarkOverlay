dd61933fd340e11fe29fefe2d4855d858ed25295
chernovmo <maxim.chernov@softline.com>
2020-01-14 17:46:01 +0300

    Рефакторинг

---
+++ Interfaces/IMusicService.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.Wave;
+using System.Collections.Generic;
+
+namespace Overlay_Watermark.Interfaces
+{
+    public interface IMusicService
+    {
+        /// <summary>
+        /// Создание трека
+        /// </summary>
+        /// <param name="musicList">Список треков</param>
+        /// <param name="musicBreak">Длительность паузы между треками</param>
+        ISampleProvider Create(List<string> musicList, double musicBreak);
+
+        /// <summary>
+        /// Получение длительности трека
+        /// </summary>
+        double GetLenght(List<string> musicList, double musicBreak);
+    }
+}
